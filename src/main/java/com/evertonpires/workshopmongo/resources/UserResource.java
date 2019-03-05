cbc317361b30f8ea3ebf297b57cef28dfceb0fe4
Everton Pires <epe.masork@hotmail.com>
2019-03-05 18:48:21 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/evertonpires/workshopmongo/resources/UserResource.java
+++ src/main/java/com/evertonpires/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.evertonpires.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.evertonpires.workshopmongo.domain.User;
+import com.evertonpires.workshopmongo.dto.UserDTO;
 import com.evertonpires.workshopmongo.service.UserService;
 
 @RestController//classe de recursuo Rest
@@ -19,13 +21,17 @@ public class UserResource {//controlador rest
 	private UserService service;//controlador rest, acessa o serviço
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>>findAll(){
+	public ResponseEntity<List<UserDTO>>findAll(){
 		
 	List<User> list = service.findAll();//busca os usuarios no banco de dados e guarda na Lista "list"
+	List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+	//map, pega cada obj " X "  da list original, e retorna um new UserDTO, passando "X" como argumento
+	
+	
 	//devolve a lista na resposta da requisicao
 	return ResponseEntity
 			.ok()//Método que instacia o ResponseEntity já com o codigo de resposta HTTP que a resposta ocorreu com sucesso
-			.body(list);// definir o corpo da resposta
+			.body(listDto);// definir o corpo da resposta
 	
 	//ResponseEntity :  Retorna um objeto sofisticado, que encapsula uma estrutura necessaria
 	//para retornar respostas HTTP já com possiveis cabeçalhos , erros etc
