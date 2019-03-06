b9578e1e2392df3a5ddd7405202a348cb7aac8d9
Everton Pires <epe.masork@hotmail.com>
2019-03-05 21:18:34 -0300

    Obtendo um post por ID

---
+++ src/main/java/com/evertonpires/workshopmongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.evertonpires.workshopmongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.evertonpires.workshopmongo.domain.Post;
+import com.evertonpires.workshopmongo.repository.PostRepository;
+import com.evertonpires.workshopmongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	@Autowired//instanciar automaticamente um objeto na classe UserService
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(String.format("Objeto não encontrado",id)));
+		}
+
+}
